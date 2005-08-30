cc38f7bf6a7fe0f5ccd924f683e48bf5ea7dc1e3
janvier <janvier@f5760b0a-27ff-0310-b7b5-dbb7ad6c737c>
2005-08-30 10:11:41 +0000

    Added nullptr and SocketException.

---
+++ trunk/src/frog/SocketException.h
@@ -0,0 +1,109 @@
+// C++ header file -------------------------------------------------------//
+//   Frog Framework - A useful framework for C++ applications.
+//   Copyright (C) 2005 by Janvier D. Anonical <janvier@users.berlios.de>
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+//
+//   You should have received a copy of the GNU Lesser General Public
+//   License along with this library; if not, write to the Free Software
+//   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+//   MA  02110-1301  USA
+//------------------------------------------------------------------------//
+
+
+#ifndef FROG_SYS_SOCKETEXCEPTION_H
+#define FROG_SYS_SOCKETEXCEPTION_H
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <frog/IOException.h>
+
+/**
+ * @namespace frog The Frog Framework is library of C++ classes,
+ * interfaces, and value types.
+ */
+namespace frog
+{
+	/**
+	 * @namespace frog::sys Contains fundamental classes and base classes that
+	 * define commonly used value and data types, interfaces, attributes,
+	 * and processing exceptions.
+	 */
+	namespace sys
+	{
+		/**
+		 * @namespace frog::sys::net Provides a simple programming
+		 * interface for many of the protocols used on networking
+		 * applications.
+		 */
+		namespace net
+		{
+			/**
+			 * Thrown to indicate that there is an error in the underlying protocol,
+			 * such as a TCP error.
+			 *
+			 * <HR>
+			 * <H3>Inherits from:</H3>
+			 *     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			 *     IOException
+			 * <HR>
+			 */
+			class SocketException : public IOException
+			{
+			  public:
+				  /**
+				   * Constructs a new exception.
+				   */
+				  explicit SocketException() throw()
+					  : IOException("SocketException") {}
+
+				  /**
+				   * Constructs a new exception with the specified description.
+				   * @param[in] description The description of this exception
+				   */
+				  explicit SocketException(const std::string& description) throw()
+					  : IOException(description) {}
+
+				  /**
+				   * Constructs a new exception with the specified description.
+				   * @param[in] description The description of this exception
+				   */
+				  explicit SocketException(const char* description) throw()
+					  : IOException(description) {}
+				  /**
+				   * Does nothing.
+				   */
+				  virtual ~SocketException() throw() {}
+
+				  /**
+				   * Returns the textual representation of the current object. In
+				   * general, the toString function returns a string that
+				   * "textually represent" the object. The result should be a
+				   * concise but informative representation that is easy for a person
+				   * to read. It is recommended that all subclasses override this
+				   * method otherwise it can be misleading if this function is called
+				   * on a sublcass of Object.
+				   */
+				  virtual std::string toString() const throw()
+				  {
+					  return std::string("frog::sys::SocketException") + ": " +
+						  std::string(getDescription());  
+				  }
+			}; // SocketException cls
+		} // net ns
+	} // sys ns
+} // frog ns
+
+#endif // FROG_SYS_SOCKETEXCEPTION_H
+
---
+++ trunk/src/frog/nullptr.h
@@ -0,0 +1,72 @@
+// C++ header file -------------------------------------------------------//
+//   Frog Framework - A useful framework for C++ applications.
+//   Copyright (C) 2005 by Janvier D. Anonical <janvier@users.berlios.de>
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+//
+//   You should have received a copy of the GNU Lesser General Public
+//   License along with this library; if not, write to the Free Software
+//   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+//   MA  02110-1301  USA
+//------------------------------------------------------------------------//
+
+
+#ifndef FROG_SYS_NULLPTR_H
+#define FROG_SYS_NULLPTR_H
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+
+/**
+ * @namespace frog The Frog Framework is library of C++ classes,
+ * interfaces, and value types.
+ */
+namespace frog
+{
+	/**
+	 * @namespace frog::sys Contains fundamental classes and base classes
+	 * that define commonly used value and data types, interfaces,
+	 * attributes, and processing exceptions.
+	 */
+	namespace sys
+	{
+		/**
+		 * A null-pointer type.
+		 *
+		 * @note Adapted from Item 25 of <I>Effective C++</I> by Scott Meyers.
+		 */
+		const class
+		{
+		  public:
+			  /**
+			   * Used to convert to any type of null non-member pointer. 
+			   */
+			  template<typename T> operator T*() const { return 0; }
+
+			  /**
+			   * Used to convert to any type of null member pointer.
+			   */
+			  template<typename C, typename T> operator T C::*() const { return 0; }
+		  private:
+			  /**
+			   * Address cannot be taken.
+			   */
+			  void operator&() const;
+		} nullptr = {};
+	} // sys ns
+} // frog ns
+
+using frog::sys::nullptr;
+
+#endif // FROG_SYS_NULLPTR_H
